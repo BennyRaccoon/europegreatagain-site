f47909c920a6aa3c32469d6ce8046b2c9004f3fb
Alejandro Perez Ordoyo <theraccoonbenny@gmail.com>
2025-02-19 20:51:28 +0100

    vamos con react

--- src/api/printful.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export async function fetchProducts() {
-  const API_KEY = import.meta.env.VITE_PRINTFUL_API_KEY;
-  const response = await axios.get('https://api.printful.com/store/products', {
-    headers: { Authorization: `Bearer ${API_KEY}` }
-  });
-  return response.data;
-}
